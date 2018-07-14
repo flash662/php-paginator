4a1e2219ea6ed2e55a5d1595fba57ab124fa8573
Ivan <flash662@gmail.com>
2018-07-15 00:49:56 +0800

    Update Paginator.php

--- src/JasonGrimes/Paginator.php
+++ src/JasonGrimes/Paginator.php
@@ -281,7 +281,7 @@ class Paginator
 
         $html = '<ul class="pagination">';
         if ($this->getPrevUrl()) {
-            $html .= '<li class="page-item"><a href="' . htmlspecialchars($this->getPrevUrl()) . '">&laquo; '. $this->previousText .'</a></li>';
+            $html .= '<li class="page-item"><a class="page-link" href="' . htmlspecialchars($this->getPrevUrl()) . '">&laquo; '. $this->previousText .'</a></li>';
         }
 
         foreach ($this->getPages() as $page) {
@@ -293,7 +293,7 @@ class Paginator
         }
 
         if ($this->getNextUrl()) {
-            $html .= '<li class="page-item"><a href="' . htmlspecialchars($this->getNextUrl()) . '">'. $this->nextText .' &raquo;</a></li>';
+            $html .= '<li class="page-item"><a class="page-link" href="' . htmlspecialchars($this->getNextUrl()) . '">'. $this->nextText .' &raquo;</a></li>';
         }
         $html .= '</ul>';
 
